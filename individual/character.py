b7c11dbf369778f526b3569e89950a8a267d4872
Santiago Ballerini <sballerini@itba.edu.ar>
2023-08-30 17:06:45 -0300

    feat: add character class
    How its attributes are encoded in the chromosome is not implementes

---
+++ individual/character.py
@@ -0,0 +1,97 @@
+from abc import abstractmethod
+import math
+
+
+class Character:
+    def __init__(self, chromosome):
+        self.chromosome = chromosome
+
+    # -------- Encoded in chromosome TODO: implement -------- #
+
+    def get_strength_points(self):
+        pass
+
+    def get_agility_points(self):
+        pass
+
+    def get_expertise_points(self):
+        pass
+
+    def get_resistance_points(self):
+        pass
+
+    def get_health_points(self):
+        pass
+
+    def get_height(self):
+        pass
+
+    # ------- Coefficients -------- #
+
+    def get_strength_coefficient(self):
+        return 100 * math.tanh(0.01 * self.get_strength_points())
+
+    def get_agility_coefficient(self):
+        return math.tanh(0.01 * self.get_agility_points())
+
+    def get_expertise_coefficient(self):
+        return 0.6 * math.tanh(0.01 * self.get_expertise_points())
+
+    def get_resistance_coefficient(self):
+        return math.tanh(0.01 * self.get_resistance_points())
+
+    def get_health_coefficient(self):
+        return 100 * math.tanh(0.01 * self.get_health_points())
+
+    # ------- Attack and Defense modifiers -------- #
+
+    def get_attack_modifier(self):
+        height = self.get_height()
+        return 0.5 - (3 * height - 5) ** 4 + (3 * height - 5) ** 2 + height / 2
+
+    def get_defense_modifier(self):
+        height = self.get_height()
+        return 2 + (3 * height - 5) ** 4 - (3 * height - 5) ** 2 - height / 2
+
+    # ------- Attack and Defense -------- #
+
+    def get_attack(self):
+        return (
+            (self.get_agility_coefficient() + self.get_expertise_coefficient())
+            * self.get_strength_coefficient()
+            * self.get_attack_modifier()
+        )
+
+    def get_defense(self):
+        return (
+            (self.get_resistance_coefficient() + self.get_expertise_coefficient())
+            * self.get_health_coefficient()
+            * self.get_defense_modifier()
+        )
+
+    # ------- Performance -------- #
+
+    @abstractmethod
+    def get_performance(self):
+        pass
+
+
+# ------- Character types -------- #
+class Warrior(Character):
+    def get_performance(self):
+        return 0.6 * self.get_attack() + 0.4 * self.get_defense()
+
+
+class Archer(Character):
+    def get_performance(self):
+        return 0.9 * self.get_attack() + 0.1 * self.get_defense()
+
+
+class Defender(Character):
+    def get_performance(self):
+        return 0.1 * self.get_attack() + 0.9 * self.get_defense()
+
+
+class Infiltrator(Character):
+    def get_performance(self):
+        return 0.7 * self.get_attack() + 0.3 * self.get_defense()
