a2db8a0103dd678a9367440d30f9d0de8f7e24c5
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-09-02 13:50:18 -0300

    feat: anular crossover

--- genetic_operators/crossover_methods.py
+++ genetic_operators/crossover_methods.py
@@ -9,8 +9,17 @@ def __assignation(from_point, to_point, child1, child2, chromosome1, chromosome2
 def anular(point, length, chromosome1, chromosome2):
     child1 = child2 = []
 
+    if point+length > CHROMOSOME_LENGTH:
+        excess_length= point+length-CHROMOSOME_LENGTH
+        __assignation(0, excess_length, child2, child1, chromosome1, chromosome2)
+        __assignation(excess_length+1, point, child1, child2, chromosome1, chromosome2)
+        __assignation(point+1, CHROMOSOME_LENGTH, child2, child1, chromosome1, chromosome2)
+    else:
+        __assignation(0, point, child1, child2, chromosome1, chromosome2)
+        __assignation(point+1, point+1+length, child2, child1, chromosome1, chromosome2)
+        __assignation(point+1+length, CHROMOSOME_LENGTH, child1, child2, chromosome1, chromosome2)
     
-    pass
+    return child1, child2
 
 
 def point(point, chromosome1, chromosome2):
