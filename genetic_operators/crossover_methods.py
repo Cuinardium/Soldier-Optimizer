1f9c5254457766b72d5b2463e3c964f224769878
Franco Bosetti <francobosetti@live.com.ar>
2023-09-01 21:00:47 -0300

    refactor: modularization for assignation
    fixes point not being considered in direct assignation

--- genetic_operators/crossover_methods.py
+++ genetic_operators/crossover_methods.py
@@ -1,24 +1,31 @@
 CHROMOSOME_LENGTH = 6
 
 
+def __direct_assignation(from_point, to_point, child1, child2, chromosome1, chromosome2):
+    for i in range(from_point, to_point + 1):
+        child1[i] = chromosome1[i]
+        child2[i] = chromosome2[i]
+
+
+def __inverse_assignation(from_point, to_point, child1, child2, chromosome1, chromosome2):
+    for i in range(from_point, to_point + 1):
+        child2[i] = chromosome1[i]
+        child1[i] = chromosome2[i]
+
 def anular():
     pass
 
 
 def point(point, chromosome1, chromosome2):
     child1 = child2 = []
-    for i in range(point):
-        child1[i] = chromosome1[i]
-        child2[i] = chromosome2[i]
 
-    for i in range(point + 1, CHROMOSOME_LENGTH + 1):
-        child2[i] = chromosome1[i]
-        child1[i] = chromosome2[i]
+    __direct_assignation(0, point, child1, child2, chromosome1, chromosome2)
+    __inverse_assignation(point + 1, CHROMOSOME_LENGTH, child1, child2, chromosome1, chromosome2)
 
     return child1, child2
 
 
-def two_point():
+def two_point(point1, point2, chromosome1, chromosome2):
     pass
 
 
