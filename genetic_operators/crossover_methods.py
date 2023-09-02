8e8089fcb6c518e91d4facf6402ce607a7cd22e7
Franco Bosetti <francobosetti@live.com.ar>
2023-09-01 21:03:41 -0300

    refactor: simplified assignation

--- genetic_operators/crossover_methods.py
+++ genetic_operators/crossover_methods.py
@@ -1,16 +1,10 @@
 CHROMOSOME_LENGTH = 6
 
 
-def __direct_assignation(from_point, to_point, child1, child2, chromosome1, chromosome2):
+def __assignation(from_point, to_point, child1, child2, chromosome1, chromosome2):
     for i in range(from_point, to_point + 1):
         child1[i] = chromosome1[i]
         child2[i] = chromosome2[i]
-
-
-def __inverse_assignation(from_point, to_point, child1, child2, chromosome1, chromosome2):
-    for i in range(from_point, to_point + 1):
-        child2[i] = chromosome1[i]
-        child1[i] = chromosome2[i]
 
 def anular():
     pass
@@ -19,8 +13,8 @@ def anular():
 def point(point, chromosome1, chromosome2):
     child1 = child2 = []
 
-    __direct_assignation(0, point, child1, child2, chromosome1, chromosome2)
-    __inverse_assignation(point + 1, CHROMOSOME_LENGTH, child1, child2, chromosome1, chromosome2)
+    __assignation(0, point, child1, child2, chromosome1, chromosome2)
+    __assignation(point + 1, CHROMOSOME_LENGTH, child2, child1, chromosome1, chromosome2)
 
     return child1, child2
 
