87a587cc72e721dcf36c9295c6ab9f878a1565ac
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-09-02 13:43:46 -0300

    feat: two point crossover

--- genetic_operators/crossover_methods.py
+++ genetic_operators/crossover_methods.py
@@ -6,7 +6,10 @@ def __assignation(from_point, to_point, child1, child2, chromosome1, chromosome2
         child1[i] = chromosome1[i]
         child2[i] = chromosome2[i]
 
-def anular():
+def anular(point, length, chromosome1, chromosome2):
+    child1 = child2 = []
+
+    
     pass
 
 
@@ -20,7 +23,14 @@ def point(point, chromosome1, chromosome2):
 
 
 def two_point(point1, point2, chromosome1, chromosome2):
-    pass
+    child1 = child2 = []
+
+    __assignation(0, point1, child1, child2, chromosome1, chromosome2)
+    __assignation(point1 + 1, point2, child2, child1, chromosome1, chromosome2)
+    __assignation(point2 + 1, CHROMOSOME_LENGTH, child1, child2, chromosome1, chromosome2)
+
+    return child1, child2
+
 
 
 def uniform():
