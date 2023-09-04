7f57a32e217e3a086acf7d30a53bf9d058b709b0
nicosuarezdurrels <nicosuarez272@gmail.com>
2023-09-04 02:53:13 -0300

    feat: roulette selection

--- genetic_operators/selection_methods.py
+++ genetic_operators/selection_methods.py
@@ -1,6 +1,7 @@
 from typing import Callable, Dict
 from individual.character import Character
 from individual.fitness import FitnessFunction
+import random
 
 
 def elitism(
@@ -8,11 +9,42 @@ def elitism(
 ) -> list[Character]:
     return population
 
-
+# TODO: ver si le pasamos cantidad a seleccionar
 def roulette(
-    population: list[Character], fitness_function: FitnessFunction
+    population: list[Character], fitness_function: FitnessFunction, num_selections : int
 ) -> list[Character]:
-    return population
+    
+    # Calcula la suma total de aptitudes de todos los individuos. TODO funcion para calcular fitness
+    total_fitness = sum(character.fitness for character in population)
+    
+    cumulative_probabilities = []
+    cumulative_probability = 0.0
+    
+    # Calcula las probabilidades acumulativas para cada individuo
+    for character in population:
+        # Calcula la probabilidad relativa de selección para este individuo
+        probability = character.fitness / total_fitness
+        
+        # Agrega la probabilidad acumulativa a la lista
+        cumulative_probability += probability
+        cumulative_probabilities.append(cumulative_probability)
+        
+    # Realiza las selecciones especificadas
+    selections = []
+    for _ in range(num_selections):
+        random_value = random.random()
+        
+        # Encuentra el individuo correspondiente al valor aleatorio
+        selected_individual = None
+        for i, cumulative_probability in enumerate(cumulative_probabilities):
+            if random_value <= cumulative_probability:
+                selected_individual = population[i]
+                break
+        
+        if selected_individual is not None:
+            selections.append(selected_individual)
+    
+    return selections
 
 
 def universal(
