9e7049e223ce7df2398fadbb46c253e941e0b699
Santiago Ballerini <sballerini@itba.edu.ar>
2023-09-03 23:05:57 -0300

    fix: adapt crossover_methods to simulation contract

--- genetic_operators/crossover_methods.py
+++ genetic_operators/crossover_methods.py
@@ -1,69 +1,91 @@
+import math
 import random
 from individual.character import Character
-from typing import Callable, Dict
+from typing import Callable, Dict, Tuple
 
 CHROMOSOME_LENGTH = 6
 
+# Selects a random point in the chromosome and a random amount of genes to cross
+def anular(parent1: Character, parent2: Character) -> Tuple[Character, Character]:
+    from_point = random.randint(0, CHROMOSOME_LENGTH - 1)
+    amount = random.randint(0, math.ceil(CHROMOSOME_LENGTH / 2))
 
-def __assignation(from_point, to_point, child1, child2, chromosome1, chromosome2):
-    for i in range(from_point, to_point):
-        child1[i] = chromosome1[i]
-        child2[i] = chromosome2[i]
-
-
-def anular(point, length, chromosome1, chromosome2):
-    child1 = child2 = []
-
-    if point + length > CHROMOSOME_LENGTH:
-        excess_length = point + length - CHROMOSOME_LENGTH
-        __assignation(0, excess_length, child2, child1, chromosome1, chromosome2)
-        __assignation(excess_length, point, child1, child2, chromosome1, chromosome2)
-        __assignation(point, CHROMOSOME_LENGTH, child2, child1, chromosome1, chromosome2)
-    else:
-        __assignation(0, point, child1, child2, chromosome1, chromosome2)
-        __assignation(point, point + length, child2, child1, chromosome1, chromosome2)
-        __assignation(point + length, CHROMOSOME_LENGTH, child1, child2, chromosome1, chromosome2)
-
-    return child1, child2
+    return __cross_parents(parent1, parent2, from_point, amount)
 
 
-def point(point, chromosome1, chromosome2):
-    child1 = child2 = []
+# Selects a random point in the chromosome and crosses the genes from that point
+def point(parent1: Character, parent2: Character) -> Tuple[Character, Character]:
+    from_point = random.randint(0, CHROMOSOME_LENGTH - 1)
+    amount = CHROMOSOME_LENGTH - from_point
 
-    __assignation(0, point, child1, child2, chromosome1, chromosome2)
-    __assignation(point, CHROMOSOME_LENGTH, child2, child1, chromosome1, chromosome2)
-
-    return child1, child2
-
-
-def two_point(point1, point2, chromosome1, chromosome2):
-    child1 = child2 = []
-
-    __assignation(0, point1, child1, child2, chromosome1, chromosome2)
-    __assignation(point1, point2, child2, child1, chromosome1, chromosome2)
-    __assignation(point2, CHROMOSOME_LENGTH, child1, child2, chromosome1, chromosome2)
-
-    return child1, child2
+    return __cross_parents(parent1, parent2, from_point, amount)
 
 
-def uniform(probability, chromosome1, chromosome2):
-    child1 = child2 = []
+# Selects two random points in the chromosome and crosses the genes between them
+def two_point(parent1: Character, parent2: Character) -> Tuple[Character, Character]:
+    from_point = random.randint(0, CHROMOSOME_LENGTH - 1)
+    to_point = random.randint(0, CHROMOSOME_LENGTH - 1)
+    if from_point > to_point:
+        from_point, to_point = to_point, from_point
+
+    amount = to_point - from_point
+
+    return __cross_parents(parent1, parent2, from_point, amount)
+
+
+# For each gene, it has a probability of crossing it
+def uniform(parent1: Character, parent2: Character) -> Tuple[Character, Character]:
+    # TODO: Esto debería ser configurable?
+    probability = 0.5
+
+    child1 = Character(parent1.chromosome.copy())
+    child2 = Character(parent2.chromosome.copy())
 
     for i in range(CHROMOSOME_LENGTH):
         if random.random() < probability:
-            child2[i] = chromosome1[i]
-            child1[i] = chromosome2[i]
-        else:
-            child1[i] = chromosome1[i]
-            child2[i] = chromosome2[i]
+            child1.chromosome[i] = parent2.chromosome[i]
+            child2.chromosome[i] = parent1.chromosome[i]
 
     return child1, child2
 
 CrossOverMethod = Callable[[list[Character]], list[Character]]
-
 crossover_methods: Dict[str, CrossOverMethod] = {
-    "anular": anular,
-    "point": point,
-    "two_point": two_point,
-    "uniform": uniform,
+    "anular": lambda population: __cross_population(population, anular),
+    "point": lambda population: __cross_population(population, point),
+    "two_point": lambda population: __cross_population(population, two_point),
+    "uniform": lambda population: __cross_population(population, uniform),
 }
+
+
+# Crosses the population using the given method
+# If the population is odd, the last individual is not crossed
+def __cross_population(
+    population: list[Character],
+    method: Callable[[Character, Character], Tuple[Character, Character]],
+) -> list[Character]:
+    amount_to_cross = (
+        len(population) if len(population) % 2 == 0 else len(population) - 1
+    )
+
+    children = []
+    for i in range(0, amount_to_cross, 2):
+        child1, child2 = method(population[i], population[i + 1])
+
+        children.append(child1)
+        children.append(child2)
+
+    return children
+
+# Crosses the parents from the given point and amount
+def __cross_parents(
+    parent1: Character, parent2: Character, from_point: int, amount: int
+) -> Tuple[Character, Character]:
+    child1 = Character(parent1.chromosome.copy())
+    child2 = Character(parent2.chromosome.copy())
+
+    for i in range(amount):
+        index = (from_point + i) % CHROMOSOME_LENGTH
+        child1.chromosome[index] = parent2.chromosome[index]
+        child2.chromosome[index] = parent1.chromosome[index]
+
+    return child1, child2
