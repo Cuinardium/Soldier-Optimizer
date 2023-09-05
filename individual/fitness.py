1e25c7681ed0e71c1984618c41e68ff94dd452b6
Santiago Ballerini <sballerini@itba.edu.ar>
2023-09-04 21:54:06 -0300

    refactor: fitness function configuration with builder

--- individual/fitness.py
+++ individual/fitness.py
@@ -2,26 +2,33 @@ from individual.character import Character
 from typing import Callable
 
 
-def warrior_fitness(character: Character) -> float:
+def __warrior_fitness(character: Character) -> float:
     return 0.6 * character.get_attack() + 0.4 * character.get_defense()
 
 
-def archer_fitness(character: Character) -> float:
+def __archer_fitness(character: Character) -> float:
     return 0.9 * character.get_attack() + 0.1 * character.get_defense()
 
 
-def defender_fitness(character: Character) -> float:
+def __defender_fitness(character: Character) -> float:
     return 0.1 * character.get_attack() + 0.9 * character.get_defense()
 
 
-def infiltrator_fitness(character: Character) -> float:
+def __infiltrator_fitness(character: Character) -> float:
     return 0.7 * character.get_attack() + 0.3 * character.get_defense()
 
-FitnessFunction = Callable[[Character], float]
 
-fitness_functions = {
-    "warrior": warrior_fitness,
-    "archer": archer_fitness,
-    "defender": defender_fitness,
-    "infiltrator": infiltrator_fitness,
+FitnessFunction = Callable[[Character], float]
+__fitness_functions = {
+    "warrior": __warrior_fitness,
+    "archer": __archer_fitness,
+    "defender": __defender_fitness,
+    "infiltrator": __infiltrator_fitness,
 }
+
+
+def get_class_fitness_function(character_class: str) -> FitnessFunction:
+    if character_class not in __fitness_functions:
+        raise ValueError(f"Unknown character class: {character_class}")
+
+    return __fitness_functions[character_class]
--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 import json
 
-from individual.fitness import fitness_functions
+from individual.fitness import get_class_fitness_function
+
 from genetic_operators.selection_methods import get_selection_method
 from genetic_operators.crossover_methods import get_crossover_method
 from genetic_operators.mutation_methods import get_mutation_method
 
 from simulation import simulate
 
-stop_criteria_methods = {}
-
 
 def main():
     with open("config.json") as config_file:
         config = json.load(config_file)
 
-        # ------------ Class ------------
-        character_class = config["character_class"]
-        if character_class not in fitness_functions:
-            raise ValueError(f"Unknown character class: {character_class}")
-        fitness_function = fitness_functions[character_class]
+        # Load config
+        fitness_function = get_class_fitness_function(config["character_class"])
 
-        # ------------ Genetic Operators ------------
         selection_method = get_selection_method(config["selection"])
         crossover_method = get_crossover_method(config["crossover"])
         mutation_method = get_mutation_method(config["mutation"])
         replacement_method = get_selection_method(config["replacement"])
 
-        # ------------ Stop Criteria ------------
+        # TODO: Implement stop criteri
         stop_criteria = config["stop_criteria"]
-        if stop_criteria not in stop_criteria_methods:
-            raise ValueError(f"Unknown stop criteria: {stop_criteria}")
-        stop_criteria = stop_criteria_methods[stop_criteria]
 
-        # ------------ Population ------------
         population_size = config["population_size"]
 
-        # ------------ Simulation ------------
+        # Run simulation
         final_population = simulate(
             fitness_function,
             selection_method,
@@ -46,7 +37,7 @@ def main():
             population_size,
         )
 
-        # ------------ Results ------------
+        # Print results
         print("Final population:")
         for individual in final_population:
             print(individual)
