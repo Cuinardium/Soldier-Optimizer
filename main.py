8001826cb559e2ad212ec27b0885d5f432099d3d
Santiago Ballerini <sballerini@itba.edu.ar>
2023-09-10 21:25:54 -0300

    feat: add csv output and iterations support

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import csv
+from pathlib import Path
 
 from individual.fitness import get_class_fitness_function
 
@@ -27,24 +29,72 @@ def main():
 
         population_size = config["population_size"]
 
+        iterations = config["iterations"]
+
+        test_name = config["test_name"]
+
         # Run simulation
-        final_population, iterations = simulate(
-            fitness_function,
-            selection_method,
-            crossover_method,
-            mutation_method,
-            replacement_method,
-            stop_criteria,
-            population_size,
-        )
+        print(f"Running {test_name}")
 
-        # Print results
-        print("Final population:")
-        for individual in final_population:
-            print(individual)
-            print(f"Fitness: {fitness_function(individual)}\n")
+        simulation_results = []
+        for _ in range(iterations):
+            print(f"Running iteration {_ + 1} of {iterations}")
 
-        print(f"Number of iterations: {iterations}")
+            final_population, generations = simulate(
+                fitness_function,
+                selection_method,
+                crossover_method,
+                mutation_method,
+                replacement_method,
+                stop_criteria,
+                population_size,
+            )
+
+            best_individual = max(
+                final_population, key=lambda individual: fitness_function(individual)
+            )
+            simulation_results.append((best_individual, generations))
+
+            print(f"Best individual: {best_individual}")
+            print(f"Fitness: {fitness_function(best_individual)}")
+            print(f"Generations: {generations}")
+
+        # Save results
+        test_dir = Path(f"results/{test_name}")
+        test_dir.mkdir(parents=True, exist_ok=True)
+
+        with open(test_dir / "config.json", "w") as config_file:
+            json.dump(config, config_file, indent=4)
+
+        with open(test_dir / "results.csv", "w") as results_file:
+            results_writer = csv.writer(results_file)
+
+            results_writer.writerow(
+                [
+                    "strength",
+                    "agility",
+                    "expertise",
+                    "resistance",
+                    "health",
+                    "height",
+                    "fitness",
+                    "generations",
+                ]
+            )
+
+            for individual, generations in simulation_results:
+                results_writer.writerow(
+                    [
+                        individual.get_strength_points(),
+                        individual.get_agility_points(),
+                        individual.get_expertise_points(),
+                        individual.get_resistance_points(),
+                        individual.get_health_points(),
+                        individual.get_height(),
+                        fitness_function(individual),
+                        generations,
+                    ]
+                )
 
 
 if __name__ == "__main__":
